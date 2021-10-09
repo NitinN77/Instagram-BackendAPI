edb70717e2b4569954082ac7656a91ae08fa9b0b
VoidlessVoid7 <voidlessvoid7@gmail.com>
2021-10-09 17:31:58 +0530

    std library passwd encrpytion

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.17
 require (
 	github.com/joho/godotenv v1.4.0
 	go.mongodb.org/mongo-driver v1.7.3
-	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
 )
 
 require (
@@ -17,6 +16,7 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/text v0.3.5 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"insta-api/helper"
@@ -12,10 +13,61 @@ import (
 	"strings"
 	"time"
 
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"golang.org/x/crypto/bcrypt"
 )
+
+func GenerateKey() ([]byte, error) {
+	key := make([]byte, 32)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func Encrypt(key, data []byte) ([]byte, error) {
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = rand.Read(nonce); err != nil {
+		return nil, err
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext, nil
+}
+
+func Decrypt(key, data []byte) ([]byte, error) {
+	blockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(blockCipher)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce, ciphertext := data[:gcm.NonceSize()], data[gcm.NonceSize():]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
 
 type PostFilter struct {
 	LimitedPosts []models.Post `json:"Posts"`
@@ -24,6 +76,8 @@ type PostFilter struct {
 
 var postCollection = helper.ConnectPostsDB()
 var userCollection = helper.ConnectUsersDB()
+
+var key, _ = GenerateKey()
 
 // POST ENDPOINTS
 
@@ -62,13 +116,9 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&user)
 
 	// HASHING
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-	if err != nil {
-		log.Fatal(err)
-	}
-	user.Password = string(hash)
-
+	passwd := []byte(user.Password)
+	ciphertext, err := Encrypt(key, passwd)
+	user.Password = hex.EncodeToString(ciphertext)
 	// HASHING COMPLETE
 
 	result, err := userCollection.InsertOne(context.TODO(), user)
